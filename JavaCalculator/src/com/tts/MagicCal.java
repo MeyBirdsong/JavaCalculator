a9814f5cf2735b3b6481799a3092b793f57d0c41
MeyBirdsong <74513901+MeyBirdsong@users.noreply.github.com>
2021-01-22 10:48:58 -0600

    Add files via upload

---
+++ JavaCalculator/src/com/tts/MagicCal.java
@@ -0,0 +1,29 @@
+package com.tts;
+
+class MagicCalc {
+
+    public double squareRoot(int num) {
+        return Math.sqrt(num);
+    }
+
+    public double sin(int num) {
+        return Math.sin(Math.toRadians(num));
+    }
+
+    public double cosine(int num) {
+        return Math.cos(Math.toRadians(num));
+    }
+
+    public double tangent(int num) {
+        return Math.tan(Math.toRadians(num));
+    }
+
+    public int factorial(int num) {
+        int fact = 1;
+        for (int i = 2; i <= num; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+}
+
